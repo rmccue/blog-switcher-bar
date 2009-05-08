4a6a8cd05c39cdc53afe8b4195c7879208ad0239
Ryan McCue <ryanmccue@cubegames.net>
2009-05-08 18:40:19 +1000

    Stripped and begun dev

--- blog-switcher-bar.php
+++ blog-switcher-bar.php
@@ -2,15 +2,16 @@
 
 **************************************************************************
 
-Plugin Name:  WordPress Admin Bar
+Plugin Name:  Blog Switcher Bar
 Plugin URI:   http://github.com/rmccue/blog-switcher-bar
 Description:  Based on Viper007Bond's WordPress Admin Bar.
-Version:      3.1.5
-Author:       Viper007Bond
-Author URI:   http://www.viper007bond.com/
+Version:      0.1
+Author:       Ryan McCue
+Author URI:   http://ryanmccue.info/
 
 **************************************************************************
 
+Copyright (C) 2009 Ryan McCue
 Copyright (C) 2008 Viper007Bond
 
 This program is free software: you can redistribute it and/or modify
@@ -29,9 +30,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **************************************************************************/
 
 class BlogSwitcherBar {
-	var $version = '3.1.5';
+	var $version = '0.1';
 	var $menu = array();
-	var $submenu = array();
 	var $folder;
 
 	function BlogSwitcherBar() {
@@ -41,20 +41,6 @@ class BlogSwitcherBar {
 	function __construct() {
 		// This version only supports WP 2.7+
 		if ( !function_exists('wp_list_comments') ) return;
-
-		// Load up the localization file if we're using WordPress in a different language
-		// Place it in this plugin's folder and name it "wordpress-admin-bar-[value in wp-config].mo"
-		load_plugin_textdomain( 'wordpress-admin-bar', FALSE, '/wordpress-admin-bar' );
-
-		//add_filter( 'plugin_action_links', array(&$this, 'AddPluginActionLink'), 10, 2 );
-		add_action( 'load-settings_page_wordpress-admin-bar', array(&$this, 'SettingsPageInit') );
-		add_action( 'admin_head-settings_page_wordpress-admin-bar', array(&$this, 'AdminCSS') );
-		add_action( 'admin_post_wordpress-admin-bar', array(&$this, 'HandleFormPOST') );
-
-		// Modify the menu array a little to make it better
-		add_filter( 'wpabar_menuitems', array(&$this, 'AddSingleEditLink') );
-		add_filter( 'wpabar_menuitems', array(&$this, 'CommentsAwaitingModCount') );
-		add_filter( 'wpabar_menuitems', array(&$this, 'PluginsUpdateCount') );
 
 		$this->folder = plugins_url('blog-switcher-bar');
 
@@ -69,29 +55,16 @@ class BlogSwitcherBar {
 	function SetupMenu() {
 		if ( !empty($this->menu) ) return;
 
-		global $wpdb, $menu, $submenu, $_wp_submenu_nopriv;
-
-		require_once( ABSPATH . 'wp-admin/includes/admin.php' );
-		require_once( ABSPATH . 'wp-admin/menu.php' );
-
-		// The top-level menus
-		foreach( $menu as $id => $menuitem ) {
-			if ( empty($menuitem[2]) ) continue;
-			$custom = ( !empty($menuitem[3]) ) ? TRUE : FALSE;
-			$this->menu[$menuitem[2]] = array( 0 => array( 'id' => $id, 'title' => $menuitem[0], 'custom' => $custom ) );
-		}
-
-		// All children menus
-		foreach( $submenu as $parent => $submenuitem ) {
-			foreach( $submenuitem as $id => $item ) {
-				$custom = ( isset($item[3]) ) ? TRUE : FALSE;
-				if ( $parent == $item[2] )
-					$custom = FALSE;
-				$this->menu[$parent][$item[2]] = array( 'id' => $id, 'title' => $item[0], 'custom' => $custom );
-			}
-		}
-
-		$this->menu = apply_filters( 'wpabar_menuitems', $this->menu );
+		$this->menu = array(
+			'url' => array)
+				0 => array(
+					'id' => 0, // >39 = right-hand side
+					'title' => 'Title',
+					'url' => 'http://.../',
+				),
+				//1 => array(...) - submenu
+			),
+		);
 	}
 
 	// Output the needed CSS for the plugin
@@ -116,7 +89,6 @@ class BlogSwitcherBar {
 		<ul>
 <?php
 
-			$first = TRUE;
 			$switched = FALSE;
 
 			foreach( $this->menu as $topstub => $menu ) {
@@ -125,22 +97,15 @@ class BlogSwitcherBar {
 					$switched = TRUE;
 				}
 
-				if ( isset($this->settings['hide'][$topstub][0]) && ( 'options-general.php' !== $topstub || !is_admin() ) ) continue;
+				if ( isset($this->settings['hide'][$topstub][0]) ) continue;
 
 				if ( 1 == count($menu) ) {
 					echo '			<li class="bsbar-menu_';
-					if ( TRUE === $menu[0]['custom'] ) echo 'admin-php_';
 					echo str_replace( '.', '-', $topstub );
 
-					if ( TRUE == $first ) {
-						echo ' bsbar-menu-first';
-						$first = FALSe;
-					}
-
-					echo '"><a href="' . admin_url( $topstub ) . '">' . $menu[0]['title'] . "</a></li>\n";
+					echo '"><a href="' . $menu[0]['url'] '">' . $menu[0]['title'] . "</a></li>\n";
 				} else {
 					echo '			<li class="bsbar-menu_';
-					if ( TRUE === $menu[0]['custom'] ) echo 'admin-php_';
 					echo str_replace( '.', '-', $topstub );
 
 					if ( TRUE == $first ) {
@@ -148,18 +113,15 @@ class BlogSwitcherBar {
 						$first = FALSe;
 					}
 
-					$url = ( TRUE === $menu[0]['custom'] ) ? 'admin.php?page=' . $topstub : $topstub;
-
-					echo ' bsbar-menupop" onmouseover="showNav(this)" onmouseout="hideNav(this)">' . "\n" . '				<a href="' . admin_url( $url ) . '"><span class="bsbar-dropdown">' . $menu[0]['title'] . "</span></a>\n				<ul>\n";
+					echo ' bsbar-menupop" onmouseover="showNav(this)" onmouseout="hideNav(this)">' . "\n" . '				<a href="' . $menu[0]['url'] . '"><span class="bsbar-dropdown">' . $menu[0]['title'] . "</span></a>\n				<ul>\n";
 
 					foreach( $menu as $submenustub => $submenu ) {
 						if ( 0 === $submenustub || ( !empty($this->settings['hide'][$topstub]) && TRUE === $this->settings['hide'][$topstub][$submenustub] && ( 'wordpress-admin-bar' !== $submenustub || !is_admin() ) ) )
 							continue;
 
 						$parent = ( TRUE === $menu[0]['custom'] ) ? 'admin.php' : $topstub;
-						$url = ( TRUE === $submenu['custom'] ) ? $parent . '?page=' . $submenustub : $submenustub;
 
-						echo '					<li><a href="' . admin_url( $url ) . '">' . $submenu['title'] . "</a></li>\n";
+						echo '					<li><a href="' . $submenu['url'] . '">' . $submenu['title'] . "</a></li>\n";
 					}
 
 					echo "				</ul>\n			</li>\n";
